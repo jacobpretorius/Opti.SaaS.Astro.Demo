0d9b0d8b3e48598b4411fed3edd08126760c968d
Jacob Pretorius <17687841+jacobpretorius@users.noreply.github.com>
2025-01-02 10:11:19 +0000

    Update astro

--- static-site/astro.config.mjs
+++ static-site/astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://opti-saas-astro-static-demo.vercel.app',
+  site: 'https://opti-saas-astro-static-demo.vercel.app',
+  adapter: vercel(),
 	integrations: [sitemap()],
 });
